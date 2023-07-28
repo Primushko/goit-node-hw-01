6d1d33976b6d0186ae987ac52f7025afc81586e3
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-07-28 17:41:06 +0300

    17/40______28/07/23

--- contacts.js
+++ contacts.js
@@ -2,11 +2,30 @@ const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "db/contacts.json");
-
-async function listContacts() {
+// уникнете повторення блоку try...catch у кожній функції за допомогою
+// допоміжних функцій: readContactsFile() та writeContactsFile()
+async function readContactsFile() {
   try {
     const data = await fs.readFile(contactsPath);
     return JSON.parse(data);
+  } catch (error) {
+    throw new Error("помилка при зчитуванні файлу контактів.");
+  }
+}
+
+async function writeContactsFile(contacts) {
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  } catch (error) {
+    throw new Error("Помилка при записі у файл контактів.");
+  }
+}
+/////////////////////////////////////////////////////////////////////////
+async function listContacts() {
+  try {
+    return await readContactsFile();
+    // const data = await fs.readFile(contactsPath);
+    // return JSON.parse(data);
   } catch (error) {
     console.log(error.message);
   }
@@ -14,7 +33,8 @@ async function listContacts() {
 
 async function getContactById(contactId) {
   try {
-    const contacts = await listContacts();
+    const contacts = await readContactsFile();
+    // const contacts = await listContacts();
     const result = contacts.find((item) => item.id === contactId);
     return result || null;
   } catch (error) {
@@ -24,13 +44,15 @@ async function getContactById(contactId) {
 
 async function removeContact(contactId) {
   try {
-    const contacts = await listContacts();
+    const contacts = await readContactsFile();
+    // const contacts = await listContacts();
     const indexContact = contacts.findIndex((item) => item.id === contactId);
     if (indexContact === -1) {
       return null;
     }
     const [delContact] = contacts.splice(indexContact, 1);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    await writeContactsFile(contacts);
+    // await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
     return delContact;
   } catch (error) {
     console.log(error.message);
@@ -39,7 +61,8 @@ async function removeContact(contactId) {
 
 async function addContact(name, email, phone) {
   try {
-    const contacts = await listContacts();
+    // const contacts = await listContacts();
+    const contacts = await readContactsFile();
     const newContact = {
       id: nanoid(),
       name,
@@ -47,7 +70,8 @@ async function addContact(name, email, phone) {
       phone,
     };
     contacts.push(newContact);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    await writeContactsFile(contacts);
+    // await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
     return newContact;
   } catch (error) {
     console.log(error.message);
